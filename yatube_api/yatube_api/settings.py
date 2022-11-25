e8041317220cf838dc6875e71e3b9db0407a802d
taube-a <Taube.Anastasia@yandex.ru>
2022-11-25 12:25:38 +0300

    feat: add new settings: AUTHENTICATION. Install new libs into requirements.txt: django-filter, djoser, djangorestframework-simplejwt

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -15,7 +16,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
+    'djoser',
+    'django_filters',
+
     'api',
     'posts',
 ]
@@ -90,4 +95,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_READER_TYPES': ('Bearer',),
 }
