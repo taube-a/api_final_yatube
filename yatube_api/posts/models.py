9e392e589bcbe579f707cfc7fb206423e0a060fd
taube-a <Taube.Anastasia@yandex.ru>
2022-11-27 16:18:48 +0300

    feat: add verbose_name to model"s fields

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -22,46 +22,74 @@ class Group(models.Model):
 class Post(models.Model):
     """Таблица Посты."""
     text = models.TextField(
-        'Текст поста', help_text='Введите текст поста')
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+        'Текст поста',
+        help_text='Введите текст поста')
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='posts')
     group = models.ForeignKey(
         Group,
+        verbose_name='Группа',
         on_delete=models.SET_NULL,
         related_name='posts',
         blank=True,
         null=True,
-        verbose_name='Группа',
         help_text='Выберите группу')
     image = models.ImageField(
-        'Картинка', upload_to='posts/', null=True, blank=True)
+        'Картинка',
+        upload_to='posts/',
+        null=True,
+        blank=True)
+
+    class Meta:
+        verbose_name = 'пост'
+        verbose_name_plural = 'посты'
 
     def __str__(self):
-        return self.text
+        return f'"{self.text[:15]}..."'
 
 
 class Comment(models.Model):
     """Таблица Комментарии.."""
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='comments')
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
-    text = models.TextField()
+        Post,
+        verbose_name='Пост',
+        on_delete=models.CASCADE,
+        related_name='comments')
+    text = models.TextField(
+        'Текст поста')
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True)
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'комментарии'
 
 
 class Follow(models.Model):
     """Таблица Подписки."""
     user = models.ForeignKey(
         User,
+        verbose_name='Пользователь',
         on_delete=models.CASCADE,
         blank=True,
         null=True,
         related_name='follower')
     following = models.ForeignKey(
         User,
+        verbose_name='Подписка',
         on_delete=models.CASCADE,
         blank=True,
         null=True,
@@ -75,4 +103,4 @@ class Follow(models.Model):
                 fields=('user', 'following'), name='unique_following')]
 
     def __str__(self):
-        return f'{self.user} подписан на пользователя {self.following}'
+        return f'{self.user_id} подписан на пользователя {self.following_id}'
