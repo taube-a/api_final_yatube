01f16958c8bbb5ab781b79d15e1088e735bccd6c
taube-a <Taube.Anastasia@yandex.ru>
2022-11-27 15:05:43 +0300

    refactor: move v1/ into app"s module to support different api"s versions

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
 
+app_name = 'api'
 
 v1_router = DefaultRouter()
 v1_router.register('posts', PostViewSet, basename='posts')
@@ -10,4 +11,4 @@ v1_router.register('groups', GroupViewSet, basename='groups')
 v1_router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
                    basename='comments')
 v1_router.register('follow', FollowViewSet, basename='follow')
-urlpatterns = [path('', include(v1_router.urls)), ]
+urlpatterns = [path('v1/', include(v1_router.urls)), ]
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -4,9 +4,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-    path('api/v1/', include('djoser.urls')),
-    path('api/v1/', include('djoser.urls.jwt')),
+    path('api/', include('api.urls', namespace='api')),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.jwt')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
