44b3c07b423222c20079553a5dda21132e2d254c
taube-a <Taube.Anastasia@yandex.ru>
2022-11-29 17:31:14 +0300

    refactor: add jwt at v1 api

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import DefaultRouter
 
 from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
 
-app_name = 'api'
 
 v1_router = DefaultRouter()
 v1_router.register('posts', PostViewSet, basename='posts')
@@ -11,4 +10,8 @@ v1_router.register('groups', GroupViewSet, basename='groups')
 v1_router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
                    basename='comments')
 v1_router.register('follow', FollowViewSet, basename='follow')
-urlpatterns = [path('v1/', include(v1_router.urls)), ]
+
+urlpatterns = [path('v1/', include(v1_router.urls)),
+               path('v1/', include('djoser.urls')),
+               path('v1/', include('djoser.urls.jwt')),
+               ]
