681cca66569cbc0674d96a9f59f2e53637b934f1
taube-a <Taube.Anastasia@yandex.ru>
2022-12-01 09:29:29 +0300

    refactor: put jwt-token patterns in a separate list

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -11,7 +11,8 @@ v1_router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
                    basename='comments')
 v1_router.register('follow', FollowViewSet, basename='follow')
 
+jwtpatterns = [path('', include('djoser.urls')),
+               path('', include('djoser.urls.jwt')), ]
+
 urlpatterns = [path('v1/', include(v1_router.urls)),
-               path('v1/', include('djoser.urls')),
-               path('v1/', include('djoser.urls.jwt')),
-               ]
+               path('v1/', include(jwtpatterns)), ]
