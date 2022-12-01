f910c108bf8bb3531c2d4f35e17af03411211548
taube-a <Taube.Anastasia@yandex.ru>
2022-12-01 08:46:06 +0300

    refactor: delete extra url path from main urls.py

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -5,8 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/', include('djoser.urls')),
-    path('api/', include('djoser.urls.jwt')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
